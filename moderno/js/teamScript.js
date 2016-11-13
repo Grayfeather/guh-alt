88e1a3c7963d913a1e064a7a01ac5a6f54c8f91c
Tomasz Czernuszenko <tczernuszenko@gmail.com>
2016-11-13 00:01:46 +0000

    Create teamScript.js

---
+++ moderno/js/teamScript.js
@@ -0,0 +1,18 @@
+var projectName = "";
+var longDescription = "";
+var shortDescription = "";
+var foundationDate = "";
+var audience = "";
+var teamSize = -1;
+var averageExperience = -1;
+var currentFunding = -1;
+
+
+function getInput(){
+   projectName = document.getElementById("name").value;
+   longDescription = document.getElementById("decrLong").value;
+   shortDescription = document.getElementById("exSummary").value;
+   foundationDate = document.getElementById("dateStart").value;
+   audience = document.getElementById("name").value;
+   
+}
